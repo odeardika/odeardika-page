6c19ad0d42f5fd05de4fe7d7c447bcc90013f365
odeardika <dirfanardika@gmail.com>
2023-07-21 15:59:59 +0800

    Done with contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -11,20 +11,22 @@ const Contact = ({email}) => {
             </div>
         </div>
         <form>
-            <div className='w-full px-4 mb-8'>
-                <label for='name' className='text-base font-bold text-primary'>Name</label>
-                <input type="text" id='name' className='w-full mt-2 bg-slate-200 text-dark p-3 rounded-sm focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary' />    
-            </div>
-            <div className='w-full px-4 mb-8'>
-                <label for='email' className='text-base font-bold text-primary'>Email</label>
-                <input type="email" id='email' className='w-full mt-2 bg-slate-200 text-dark p-3 rounded-sm focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary' />
-            </div>
-            <div className='w-full px-4 mb-8'>
-                <label for='massage' className='text-base font-bold text-primary'>Massage</label>
-                <textarea type="text" id='massage' className='w-full mt-2 bg-slate-200 text-dark p-3 rounded-sm h-32 resize-none focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary' />
-            </div>
-            <div className='w-full px-4'>
-                <button className='text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500'>Send</button>
+            <div className='w-full lg:w-2/3 mx-auto'>
+                <div className='w-full px-4 mb-8'>
+                    <label for='name' className='text-base font-bold text-primary'>Name</label>
+                    <input type="text" id='name' className='w-full mt-2 bg-slate-200 text-dark p-3 rounded-sm focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary' />    
+                </div>
+                <div className='w-full px-4 mb-8'>
+                    <label for='email' className='text-base font-bold text-primary'>Email</label>
+                    <input type="email" id='email' className='w-full mt-2 bg-slate-200 text-dark p-3 rounded-sm focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary' />
+                </div>
+                <div className='w-full px-4 mb-8'>
+                    <label for='massage' className='text-base font-bold text-primary'>Massage</label>
+                    <textarea type="text" id='massage' className='w-full mt-2 bg-slate-200 text-dark p-3 rounded-sm h-32 resize-none focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary' />
+                </div>
+                <div className='w-full'>
+                    <button className='text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500'>Send</button>
+                </div>
             </div>
         </form>
         
