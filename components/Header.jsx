26264791c02be995e62a262e03c5c1e153fbb0cd
odeardika <dirfanardika@gmail.com>
2023-07-23 11:18:00 +0800

    fix image path

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,14 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import Image from 'next/image'
 
 const Header = () => {
     // detect scroll
     const [isScrool, setIsScrool] = useState(false)
     // toggle for hamburger button active or not
     const [isActive, setIsActive] = useState(false)
+    // detect windows size
+    const [isMobile, setIsMobile] = useState()
 
     
     const navbarFixed = () =>{
@@ -24,15 +25,19 @@ const Header = () => {
     }
 
     useEffect(()=>{
+        // check if the page is scrolling down
         window.addEventListener('scroll',navbarFixed )
+        //check if the page size is mobile size
+        window.addEventListener('resize',()=>{setIsMobile(window.innerWidth < 1023)})
+
     })
     
     return (
-    <header className={`${isScrool? 'navbar-fixed bg-transparent':''} absolute top-0 left-0 w-full flex items-center z-10 transition-all duration-200`}>
+    <header className={`${isScrool? 'navbar-fixed bg-transparent':''} absolute top-0 left-0 w-full flex items-center z-h-4 w-410 transition-all duration-200`}>
         <div className='container'>
             <div className='flex items-center justify-between relative'>
                 <div className='px-4'>
-                    <a href="#home" className='font-bold text-lg text-primary block py-6'><Image src='/../public/assets/ode ardika.png' alt='logo' height='50' width='70'/></a>
+                    <a href="#home" className={`font-bold text-lg block py-6 ${isScrool? 'text-primary':'text-white'}`}>odeardika</a>
                 </div>
                 <div className='flex items-center px-4'>
                     <button 
@@ -50,22 +55,22 @@ const Header = () => {
                             <li className='group'>
                                 <a 
                                 href="#home" 
-                                className=' text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Home</a>    
+                                className={`text-base text-dark pt-2 pb-1 mx-8 flex group-hover:text-primary group-hover:border-b-2 border-primary transition-all duration-100 ${isMobile? `w-fit`:`${isScrool ? '':'group-hover:text-white border-white'}`}`}>Home</a>    
                             </li>
                             <li className='group'>
                                 <a 
                                 href="#about" 
-                                className=' text-base text-dark py-2 mx-8 flex group-hover:text-primary'>About Me</a>
+                                className={`text-base text-dark pt-2 pb-1 mx-8 flex group-hover:text-primary group-hover:border-b-2 border-primary transition-all duration-100 ${isMobile? `w-fit`:`${isScrool ? '':'group-hover:text-white border-white'}`}`}>About Me</a>
                             </li>
                             <li className='group'>
                                 <a 
                                 href="#portfolio" 
-                                className=' text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Portfolio</a>
+                                className={`text-base text-dark pt-2 pb-1 mx-8 flex group-hover:text-primary group-hover:border-b-2 border-primary transition-all duration-100 ${isMobile? `w-fit`:`${isScrool ? '':'group-hover:text-white border-white'}`}`}>Portfolio</a>
                             </li>
                             <li className='group'>
                                 <a 
                                 href="#contact" 
-                                className=' text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Contact</a>
+                                className={`text-base text-dark pt-2 pb-1 mx-8 flex group-hover:text-primary group-hover:border-b-2 border-primary transition-all duration-100 ${isMobile? `w-fit`:`${isScrool ? '':'group-hover:text-white border-white'}`}`}>Contact</a>
                             </li>
                         </ul>
                     </nav>
--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ProjectSection from './icons/ProjectSection'
-import Image from 'next/image'
 import PythonIcon from './icons/PythonIcon'
 import StreamlitIcon from './icons/StreamlitIcon'
 import PHPIcon from './icons/PHPIcon'
-import BootstrapIcon from './icons/BootstrapIcon'
 import MysqlIcon from './icons/MysqlIcon'
+import clasification from 'public/assets/portfolio/klasifikasi_review.png'
+import schedule from 'public/assets/portfolio/Penjadwalan.png'
+import wifi from 'public/assets/portfolio/wisata_finder.png'
 
 const Portfolio = () => {
   return (
@@ -18,19 +19,18 @@ const Portfolio = () => {
                     <p className='font-medium text-md text-secondary md:text-lg'>
                     Below are some of the projects I recently completed. 
                     These projects come from school assignments, competitions, 
-                    other needs. To see all the projects can be seen on my <a href="https://github.com/odeardika" className='text-primary hover:opacity-70'>github</a>.</p>
+                    other needs. To see all the projects can be seen on my <a href="https://github.com/odeardika" target='_blank' className='text-primary hover:opacity-70'>github</a>.</p>
                 </div>
             </div>
             <div className='w-full px-4 flex flex-wrap justify-center'>
                 <ProjectSection 
-                imageSource='/../public/assets/portfolio/klasifikasi_review.png'
+                imageSource={clasification}
                 projectName='Review Checker'
                 imageAlt='Picture of review checker website'
                 projectDesc='Web application to classify a positive or negative review. This application uses the Naive Bayes multinomial algorithm for classifying review text. The tool we use is the Python programming language with the stremlit framework as the frontend'
                 toolIcon={[{ key: 'wifi-py',component: <PythonIcon className='icon'/>},{key: 'wifi-streamlit',component: <StreamlitIcon className='icon' />}]}/>
-                
                 <ProjectSection 
-                imageSource='/../public/assets/portfolio/wisata_finder.png'
+                imageSource={wifi}
                 projectName='Wisata Finder'
                 key='Wisata Finder'
                 imageAlt='Picture of wisata finder website'
@@ -40,7 +40,7 @@ const Portfolio = () => {
                 I act as a backend in making this application using PHP programming language and MYSQL database'
                 toolIcon={[{ key: 'wifi-php',component: <PHPIcon key='Wisata Finder' className='icon'/>},{key: 'wifi-mysql',component: <MysqlIcon key='Wisata Finder' className='icon' />}]}/>
                 <ProjectSection 
-                imageSource='/../public/assets/portfolio/Penjadwalan.png'
+                imageSource={schedule}
                 key='penjadwalan'
                 projectName='Informatics Scheduling System'
                 imageAlt='Picture of informatics scheduling system website'
