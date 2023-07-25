1c4acd48e91547430ab5c43d844f2be919aa3ee5
odeardika <dirfanardika@gmail.com>
2023-07-25 21:00:10 +0800

    use loop to show icon of social media

--- components/icons/ProjectSection.tsx
+++ components/icons/ProjectSection.tsx
@@ -11,7 +11,7 @@ const ProjectSection = ({imageSource, projectName , projectDesc, toolIcon , imag
         <h3 className='font-semibold text-xl text-dark mt-5 mb-3'>{projectName}</h3>
         <div className="flex items-center mb-3">
         {toolIcon.map((item : any) => <div className='flex'>
-          {item.component.map((icon : any) => <div key={`${icon} in ${item.key}`}><Icon className='icon' iconName={icon}/></div>)}
+          {item.component.map((icon : any) => <div key={`${icon} in ${item.key}`}><Icon key={`${icon} in ${item.key}`} className='icon' iconName={icon}/></div>)}
         </div>)}
         </div>
         <p className='font-medium text-base text-secondary '>{projectDesc}</p>
